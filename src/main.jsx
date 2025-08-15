b8f8b65f6e7cd2c2b2d06abd0b27de557205d53d
Raahim Ghori <raahimghori@gmail.com>
2025-08-15 01:22:05 -0400

    used fonts locally

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './styles/home.css'; // your original CSS
+import './styles/home.css';
+import '@fontsource/outfit/400.css';
+import '@fontsource/outfit/700.css';
+import '@fontsource/outfit/900.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
